bb20a8b891d9e510d06537f357848eab17e89416
Vansh Sachdeva <vanshsachdeva900@gmail.com>
2025-07-27 19:24:26 +0530

    newurl

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -5,7 +5,7 @@ import Stripe from "stripe"
 const stripe= new Stripe(process.env.STRIPE_SECRET_KEY)
 //placing user order for frontend
 const placeOrder=async(req,res)=>{
-    const frontend_url="http://localhost:5173";
+    const frontend_url="https://swigato-theta.vercel.app";
     try {
         const newOrder=new orderModel({
             userId:req.body.userId,
--- frontend/src/context/StoreContext.jsx
+++ frontend/src/context/StoreContext.jsx
@@ -3,7 +3,7 @@ import axios from "axios";
 export const StoreContext = createContext(null);
 const StoreContextProvider = ({ children }) => {
   const [cartItems, setCartItems] = useState({});
-  const url="http://localhost:4000";
+  const url="https://swigato-backend-zbe1.onrender.com";
   const [token, setToken] = useState("");
   const [food_list,setFoodList]= useState([]);
  
